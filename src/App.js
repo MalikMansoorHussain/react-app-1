f6420676bf7ffd91745f0df08444ebcf8e80cad1
MalikMansoorHussain <malikmansoor1990@gmail.com>
2020-06-10 15:02:33 +0500

    react parent child realtion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import Child1 from "./child1";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world 3</h1>
-
+    <div>
+    <h1>hello from parent component</h1>
+    <Child1 name="malik" age="23"/>
+    <Child1 name="mansoor" age="24"/>
+    <Child1 name="hussain" age="25"/>
     </div>
   );
 }
---
+++ src/child1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Child1(abc){
+return <h1>hello from child component my name is {abc.name} and my age is {abc.age}</h1>
+}
+
+export default Child1;
